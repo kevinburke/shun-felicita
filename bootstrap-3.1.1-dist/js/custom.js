64aec79678cb2788342140ce82bf06e81c1d35d6
Kevin Burke <kev@inburke.com>
2014-05-25 15:47:04 +0900

    initial commit

---
+++ bootstrap-3.1.1-dist/js/custom.js
@@ -0,0 +1,3 @@
+$("#book-now").click(function() {
+    $("#main-page").prepend('<div style="margin-top: 10px;" class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Successfully booked the yoga class!</strong></button></div>');
+});
